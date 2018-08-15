cc560ebe2af02072d170e3f2d5ffab21dd7e0f5d
Ornette Coleman <ornette.coleman@nike.com>
2018-08-15 10:38:58 -0700

    disabled buttons on limits

--- script.js
+++ script.js
@@ -7,33 +7,43 @@ var nBtn = document.getElementsByClassName('next')[0]
 var slide = 1
 
 function prev(){
-  console.log('prev button clicked!')
+  // console.log('prev button clicked!')
+  pBtn.classList.add('disabled')
+  nBtn.classList.remove('disabled')
   if(
     all.style.left !== "0px" && 
     all.style.left !== ""
   ){
     slide -= 1
     current.innerHTML = slide
+    pBtn.classList.remove('disabled')
     all.style.left = Number(all.style.left.slice(0,-2)) + all.offsetWidth/all.children.length + 'px'
   }
 }
 function next(){
-  console.log('next function clicked!')
+  // console.log('next function clicked!')
+  pBtn.classList.remove('disabled')
+  nBtn.classList.add('disabled')
   if(
     all.style.left !== "-" + (all.offsetWidth - all.offsetWidth/all.children.length) + "px"
   ){
     slide += 1
     current.innerHTML = slide
+    nBtn.classList.remove('disabled')
     all.style.left = Number(all.style.left.slice(0,-2)) - all.offsetWidth/all.children.length + 'px'
   }
 }
 function reset(){
-  console.log('resize event fired!')
+  // console.log('resize event fired!')
   all.style.left = "0px"
+  init()
 }
 function init(){
   current.innerHTML = slide
   total.innerHTML = all.children.length
+  pBtn.classList.add('disabled')
+  nBtn.classList.remove('disabled')
+  // nBtn.classList.remove('disabled')
 }
 
 pBtn.addEventListener('click',prev)
