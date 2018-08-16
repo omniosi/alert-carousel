ac198106777c17818fb806a4f302f06529a82b58
Ornette Coleman <ornette.coleman@nike.com>
2018-08-16 11:52:21 -0700

    all dynamic sizing added

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 var allalerts = document.getElementsByClassName('alertarea')[0]
 var cont = document.getElementsByClassName('container')[0]
 var all = document.getElementsByClassName('alerts')[0]
+var num = document.getElementsByClassName('num')[0]
 var current = document.getElementsByClassName('current')[0]
 var total = document.getElementsByClassName('total')[0]
 var pBtn = document.getElementsByClassName('prev')[0]
 var nBtn = document.getElementsByClassName('next')[0]
+var mes = document.getElementsByClassName('message')[0]
+var cBtn = document.getElementsByClassName('close')[0]
 var slide = 1
-var dataURL = 'data.json'
+
+function contDimen(){
+  // span.container{ width: calc(100% - 34px - 34px - 55px); height: 34px }
+  cont.style.width = (window.innerWidth - (pBtn.offsetWidth + nBtn.offsetWidth + num.offsetWidth) ) + 'px'
+  cont.style.height = all.offsetHeight + 'px'
+
+  // ul.alerts{ width: 500% }
+  all.style.width = all.children.length + '00%'
+
+  // ul.alerts li{ width: 20% }
+  Array.prototype.map.call(all.children,function(l){
+    return l.style.width = 100/all.children.length + '%'
+  })
+  // .alerts li .message{ width: calc(100% - 33px) }
+  Array.prototype.map.call(document.getElementsByClassName('message'),function(m){
+    return m.style.width = (m.closest('li').offsetWidth - m.nextSibling.offsetWidth) + 'px'
+  })
+
+}
 
 function loadJSON(callback) {
   var xhr = new XMLHttpRequest();
@@ -29,6 +50,7 @@ loadJSON(function(response) {
   all.innerHTML = jsonresponse.map(r => {
     return '<li class="'+r.type+'"><span class="message">'+r.message+'</span><button class="close" onclick="closeAlert(event)"><i class="fas fa-times-circle"></i></button></li>'
   }).join('')
+  contDimen()
   return jsonresponse
 });
 
@@ -37,14 +59,13 @@ function prev(){
   pBtn.classList.add('disabled')
   nBtn.classList.remove('disabled')
   if(
+    // Number(current.innerHTML) > 1 && 
     all.style.left !== "0px" && 
     all.style.left !== ""
   ){
-    // slide -= 1
     slide--
     pBtn.classList.remove('disabled')
     all.style.left = Number(all.style.left.slice(0,-2)) + all.offsetWidth/all.children.length + 'px'
-    // current.innerHTML = slide
     numUpd(slide)
   }
 }
@@ -53,15 +74,14 @@ function next(){
   pBtn.classList.remove('disabled')
   nBtn.classList.add('disabled')
   if(
+    // Number(current.innerHTML) !== Number(total.innerHTML) && 
     all.style.left !== "-" + (all.offsetWidth - all.offsetWidth/all.children.length) + "px"
   ){
-    // slide += 1
     if(slide != Number(total.innerHTML) ){
       slide++
       all.style.left = Number(all.style.left.slice(0,-2)) - all.offsetWidth/all.children.length + 'px'
       nBtn.classList.remove('disabled')
     }
-    // current.innerHTML = slide
     numUpd(slide)
   }
 }
@@ -69,7 +89,6 @@ function numUpd(num){
   current.innerHTML = num
 }
 function init(){
-  // current.innerHTML = slide
   numUpd(1)
   total.innerHTML = all.children.length
   Array.prototype.map.call(all.getElementsByTagName('li'),function(m) {
@@ -78,6 +97,7 @@ function init(){
   all.style.left = "0px"
   pBtn.classList.add('disabled')
   nBtn.classList.remove('disabled')
+  contDimen()
 }
 function closeAlert(event){
   console.log('closeAlert called!')
